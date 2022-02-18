766404c79366fbf2a8f4a446c7128f1962f4713d
Jefferson <jefernandes10@gmail.com>
2022-02-18 17:58:16 -0300

    Adicionando Projeto inicial

---
+++ src/main/java/com/project/professor/alocation/ProfessorAlocationJeffersonApplication.java
@@ -0,0 +1,16 @@
+package com.project.professor.alocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAlocationJeffersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAlocationJeffersonApplication.class, args);
+	}
+
+}
+
+
+

9fe2c5235b8eb3cc9638b62c10ddb6df7098bfa0
Jefferson <jefernandes10@gmail.com>
2022-02-22 16:49:19 -0300

    mapeamento tabela e chave primária

--- src/main/java/com/project/professor/alocation/entity/Department.java
+++ src/main/java/com/project/professor/alocation/entity/Department.java
@@ -1,5 +1,33 @@
 package com.project.professor.alocation.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+
 public class Department {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String Name;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		id = id;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
 
 }
--- src/main/java/com/project/professor/alocation/entity/Professor.java
+++ src/main/java/com/project/professor/alocation/entity/Professor.java
@@ -1,5 +1,51 @@
 package com.project.professor.alocation.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+
 public class Professor {
+	
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String cpf;
+	private Long DepartamentId;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Long getDepartamentId() {
+		return DepartamentId;
+	}
+
+	public void setDepartamentId(Long departamentId) {
+		DepartamentId = departamentId;
+	}
 
 }
